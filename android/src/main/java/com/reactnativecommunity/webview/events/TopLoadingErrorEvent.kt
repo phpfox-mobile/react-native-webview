f748244633c1926eb9f3c8fad3e15ab8d50c243f
Nguyễn Châu Thành Thiện <thiensmail96@gmail.com>
2019-03-27 08:35:23 +0700

    Update TopLoadingErrorEvent.kt

--- android/src/main/java/com/reactnativecommunity/webview/events/TopLoadingErrorEvent.kt
+++ android/src/main/java/com/reactnativecommunity/webview/events/TopLoadingErrorEvent.kt
@@ -1,8 +1,8 @@
-package com.reactnativecommunity.webview.events
+package com.reactnativecommunity.webview.events;
 
-import com.facebook.react.bridge.WritableMap
-import com.facebook.react.uimanager.events.Event
-import com.facebook.react.uimanager.events.RCTEventEmitter
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.Event;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 /**
  * Event emitted when there is an error in loading.
@@ -10,16 +10,16 @@ import com.facebook.react.uimanager.events.RCTEventEmitter
 class TopLoadingErrorEvent(viewId: Int, private val mEventData: WritableMap) :
     Event<TopLoadingErrorEvent>(viewId) {
     companion object {
-        const val EVENT_NAME = "topLoadingError"
+        const val EVENT_NAME = "topLoadingError";
     }
 
-    override fun getEventName(): String = EVENT_NAME
+    override fun getEventName(): String = EVENT_NAME;
 
-    override fun canCoalesce(): Boolean = false
+    override fun canCoalesce(): Boolean = false;
 
-    override fun getCoalescingKey(): Short = 0
+    override fun getCoalescingKey(): Short = 0;
 
     override fun dispatch(rctEventEmitter: RCTEventEmitter) =
-        rctEventEmitter.receiveEvent(viewTag, eventName, mEventData)
+        rctEventEmitter.receiveEvent(viewTag, eventName, mEventData);
 
 }
